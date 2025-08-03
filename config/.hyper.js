724a1d940732c7c0978d933521980262a4bd78be
Alexander Kondratev <kondratev.av@inbox.ru>
2025-08-03 10:53:34 +0500

    Change hyper js theme

--- config/.hyper.js
+++ config/.hyper.js
@@ -11,7 +11,7 @@ module.exports = {
     // default font size in pixels for all tabs
     fontSize: 15,
     // font family with optional fallbacks
-    fontFamily: "Fira Code, Menlo, Hack Nerd Font Mono, monospace",
+    fontFamily: "Fira Code, Hack Nerd Font Mono, monospace",
     // default font weight: 'normal' or 'bold'
     fontWeight: "normal",
     // font weight for bold characters: 'normal' or 'bold'
@@ -77,5 +77,5 @@ module.exports = {
   },
 
   // a list of plugins to fetch and install from npm
-  plugins: ["hyperterm-summon", "hyperterm-gruvbox-dark"],
+  plugins: ["hyperterm-summon", "hyper-gruv"],
 };
