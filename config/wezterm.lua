d742db43d137361651d6e0b5ef7429c267fe53be
Alexander Kondratev <101856129+kondratevdev@users.noreply.github.com>
2025-12-21 22:53:31 +0500

    Configure wezterm (#13)

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -1,24 +1,42 @@
 local wezterm = require 'wezterm'
-local act = wezterm.action
+local config = {}
 
+-- Font
+config.font = wezterm.font_with_fallback {
+  "Fira Code",
+  "Hack Nerd Font Mono",
+  "monospace",
+}
+config.font_size = 15.0
+
+-- Color scheme
+config.color_scheme = 'Ayu Dark (Gogh)'
+
+-- Window behavior
+config.window_decorations = "NONE"
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = true
+config.window_close_confirmation = "NeverPrompt"
+
+-- Key bindings
+config.keys = {
+  {
+    key = "D",
+    mods = "CMD|SHIFT",
+    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" })
+  },
+  {
+    key = "E",
+    mods = "CMD|SHIFT",
+    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" })
+  },
+}
+
+-- GUI startup
 wezterm.on("gui-startup", function(cmd)
   local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
   local gui_win = window:gui_window()
   gui_win:toggle_fullscreen()
 end)
 
-return {
-  color_scheme = "Ayu Dark (Gogh)",
-  font_size = 14.0,
-
-  window_decorations = "NONE",
-
-  hide_tab_bar_if_only_one_tab = true,
-  use_fancy_tab_bar = true,
-  window_close_confirmation = "NeverPrompt",
-
-  keys = {
-    { key = "D", mods = "CMD|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-    { key = "E", mods = "CMD|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-  },
-}
+return config
