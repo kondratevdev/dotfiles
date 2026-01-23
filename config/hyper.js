bd5a57d576337cc51844830b9b71d13908b629b2
Alexander Kondratev <101856129+kondratevdev@users.noreply.github.com>
2026-01-23 15:18:38 +0500

    comeback to hyperjs (#23)
    * comeback to hyperjs
    
    * Fix CI

---
+++ config/hyper.js
@@ -0,0 +1,81 @@
+"use strict";
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+module.exports = {
+  config: {
+    // choose either `'stable'` for receiving highly polished,
+    // or `'canary'` for less polished but more frequent updates
+    updateChannel: "stable",
+
+    // default font size in pixels for all tabs
+    fontSize: 15,
+    // font family with optional fallbacks
+    fontFamily: "Fira Code, Hack Nerd Font Mono, monospace",
+    // default font weight: 'normal' or 'bold'
+    fontWeight: "normal",
+    // font weight for bold characters: 'normal' or 'bold'
+    fontWeightBold: "bold",
+
+    // line height as a relative unit
+    lineHeight: 1,
+    // letter spacing as a relative unit
+    letterSpacing: 0,
+
+    // terminal text color under BLOCK cursor
+    cursorAccentColor: "#000",
+    // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
+    cursorShape: "BLOCK",
+    // set to `true` (without backticks and without quotes) for blinking cursor
+    cursorBlink: false,
+    // terminal background color
+    // opacity is only supported on macOS
+    backgroundColor: "#000",
+
+    // border color (window, tabs)
+    borderColor: "#333",
+    // custom padding (CSS format, i.e.: `top right bottom left`)
+    padding: "12px 14px",
+
+    // Supported Options:
+    //  1. 'SOUND' -> Enables the bell as a sound
+    //  2. false: turns off the bell
+    bell: "SOUND",
+    // An absolute file path to a sound file on the machine.
+    // bellSoundURL: '/path/to/sound/file',
+    // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
+    copyOnSelect: false,
+    // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
+    defaultSSHApp: true,
+    // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
+    // selection is present (`true` by default on Windows and disables the context menu feature)
+    quickEdit: false,
+    // choose either `'vertical'`, if you want the column mode when Option key is hold during selection (Default)
+    // or `'force'`, if you want to force selection regardless of whether the terminal is in mouse events mode
+    // (inside tmux or vim with mouse mode enabled for example).
+    macOptionSelectionMode: "vertical",
+    // Whether to use the WebGL renderer. Set it to false to use canvas-based
+    // rendering (slower, but supports transparent backgrounds)
+    webGLRenderer: true,
+    // keypress required for weblink activation: [ctrl|alt|meta|shift]
+    // todo: does not pick up config changes automatically, need to restart terminal :/
+    webLinksActivationKey: "",
+    // if `false` (without backticks and without quotes), Hyper will use ligatures provided by some fonts
+    disableLigatures: true,
+    // set to true to disable auto updates
+    disableAutoUpdates: false,
+    // set to true to enable screen reading apps (like NVDA) to read the contents of the terminal
+    screenReaderMode: false,
+    // set to true to preserve working directory when creating splits or tabs
+    preserveCWD: true,
+    // for advanced config flags please refer to https://hyper.is/#cfg
+
+    // Hyperterm-summon
+    summon: {
+      hotkey: "Ctrl+;",
+    },
+  },
+
+  // a list of plugins to fetch and install from npm
+  plugins: ["hyperterm-summon", "hyper-ayu"],
+};
